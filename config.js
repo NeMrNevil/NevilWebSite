8ba48e7fd26bbc2d76fc55652d87190a3f874f51
Nevil <mr0nevil@gmail.com>
2026-09-25 22:46:43 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,18 @@
+// Настройки сайта — меняй этот файл для обновления имени, аватарки и ссылок.
+
+const MY_NAME = "MrNevil";
+const MY_HINT = "aboba";
+const MY_AVATAR = "avatar.png";
+
+const MY_LINKS = {
+  steam:    "https://steamcommunity.com/id/mrnevil/",
+  discord:  "https://discord.com/users/X6T3dc4W",
+  namemc:   "https://namemc.com/profile/MrNevil.2",
+  youtube:  "https://www.youtube.com/@MrNev1l",
+  telegram: "https://t.me/nevil_tganal",
+  twitch:   "https://www.twitch.tv/mrnevil_",
+  kick:     "https://kick.com/ТВОЙ_НИК",
+  github:   "https://github.com/ТВОЙ_НИК",
+  twitter:  "https://x.com/ТВОЙ_НИК",
+  vk:       "https://vk.com/ТВОЙ_НИК",
+};
